dc4f70256c2ab166a3b4a41bd4c11c157f16cad7
Shihan Chowdhury <hackedgdown@gmail.com>
2023-06-01 23:13:45 +0100

    78-11 (super advanced) Make user api secure and verify admin

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -5,12 +5,13 @@ import useAxiosSecure from './useAxiosSecure';
 
 const useCart = () => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     // const token = localStorage.getItem('access-token');
     const [axiosSecure] = useAxiosSecure();
 
     const { isLoading, data: cart = [], refetch } = useQuery({
         queryKey: ['carts', user?.email],
+        enabled: !loading,
         // queryFn: async () => {
         //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
         //         headers: {
